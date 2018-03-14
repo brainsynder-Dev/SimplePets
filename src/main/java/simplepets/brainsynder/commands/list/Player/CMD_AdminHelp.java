c3738d1f88b55f069d4c2561e961dae5e4f6c86f
brainsynder <briansnyder498@gmail.com>
2018-03-14 17:11:30 -0400

    Added `/pet generator` command

---
+++ src/main/java/simplepets/brainsynder/commands/list/Player/CMD_AdminHelp.java
@@ -0,0 +1,145 @@
+package simplepets.brainsynder.commands.list.Player;
+
+import org.bukkit.entity.Player;
+import simplepets.brainsynder.PetCore;
+import simplepets.brainsynder.commands.PetCommand;
+import simplepets.brainsynder.commands.annotations.*;
+import simplepets.brainsynder.menu.menuItems.base.MenuItem;
+import simplepets.brainsynder.pet.PetDefault;
+import simplepets.brainsynder.reflection.ReflectionUtil;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Calendar;
+
+@CommandName(name = "generator")
+@CommandPermission(permission = "generate")
+@CommandUsage(usage = "<permissions|types>")
+@CommandDescription(description = "Generates a file that can contain Permissions, Pet Types, Commands, ETC...")
+public class CMD_AdminHelp extends PetCommand<Player> {
+    @Override
+    public void onCommand(Player sender, String[] args) {
+        if (args.length == 0) {
+            sendUsage(sender);
+            return;
+        }
+        PetCore plugin = PetCore.get();
+
+        if (args[0].equalsIgnoreCase("permissions")) {
+            generatePermissions ();
+            sender.sendMessage("Permissions have been generated into a file");
+            return;
+        }
+
+        if (args[0].equalsIgnoreCase("types")) {
+            StringBuilder builder = new StringBuilder();
+            plugin.getTypeManager().getTypes().forEach(type -> {
+                builder.append(type.getConfigName()).append("\n");
+                builder.append("   Enabled:").append(type.isEnabled()).append("\n");
+                builder.append("   Supported:").append(type.isSupported()).append("\n");
+                builder.append("   Supported Version:").append(type.getAllowedVersion()).append("\n");
+                builder.append("   Ride Speed:").append(type.getRideSpeed()).append("\n");
+                builder.append("   Walk Speed:").append(type.getSpeed()).append("\n");
+                builder.append("   Default Sound:").append(type.getDefaultSound().name()).append("\n");
+                builder.append("   Sound:").append(type.getSound().name()).append("\n");
+                builder.append("   Display Name:").append(type.getDisplayName()).append("\n");
+            });
+            new Logger("Pet Types").log(new File(plugin.getDataFolder().toString() + File.separator + "Generated Files" + File.separator), builder.toString());
+            sender.sendMessage("Pet Types have been generated into a file");
+        }
+    }
+
+    private class Logger {
+        private Calendar calendar = Calendar.getInstance();
+        private String fileName;
+
+        public Logger(String fileName) {
+            this.fileName = fileName;
+        }
+
+        public void log(File folder, String message) {
+            try {
+                if (!folder.exists()) folder.mkdirs();
+                File saveTo = new File(folder, fileName + ".txt");
+                if (saveTo.exists()) saveTo.delete();
+                saveTo.createNewFile();
+
+                FileWriter fw = new FileWriter(saveTo, true);
+                PrintWriter pw = new PrintWriter(fw);
+                pw.println(message);
+                pw.flush();
+                pw.close();
+            } catch (IOException var7) {
+                var7.printStackTrace();
+            }
+        }
+    }
+
+    private MenuItem getItem(PetDefault type, Class<? extends MenuItem> clazz) {
+        try {
+            return ReflectionUtil.initiateClass(ReflectionUtil.fillConstructor(clazz, PetDefault.class), type);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    private void generatePermissions () {
+        PetCore plugin = PetCore.get();
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("This list is not final More stuff might be added: ").append("\n\n");
+        builder.append("All Permissions: ").append("\n");
+        Arrays.asList(
+                "Pet.*", "Pet.PetToMount", "Pet.PetToHat",
+                "Pet.type.*", "Pet.type.passive", "Pet.type.hostile"
+        ).forEach(permission -> builder.append("  - ").append(permission).append("\n"));
+
+
+        builder.append("Pet Renaming Permissions: ").append("\n");
+        Arrays.asList(
+                "Pet.name",
+                "Pet.name.color", "Pet.name.magic",
+                "Pet.name.bypass", "Pet.name.bypassLimit",
+                "Pet.name.*"
+        ).forEach(permission -> builder.append("  - ").append(permission).append("\n"));
+
+        builder.append("Other Permissions: ").append("\n");
+        Arrays.asList(
+                "Pet.economy.bypass"
+        ).forEach(permission -> builder.append("  - ").append(permission).append("\n"));
+
+        builder.append("Command Permissions: ").append("\n");
+        builder.append("  - Pet.commands.* \n");
+        builder.append("  - Pet.commands.summon.other \n");
+        builder.append("  - Pet.commands.remove.other \n");
+        builder.append("  - Pet.commands.inv.other \n");
+        builder.append("  - Pet.commands.remove.other \n");
+        plugin.getSPCommand().commands.forEach(command -> {
+            if (!command.getClass().isAnnotationPresent(Console.class))
+                if (command.needsPermission())
+                    builder.append("  - ").append(command.getPermission()).append("\n");
+        });
+
+
+        builder.append("Pet Permissions: ").append("\n");
+        plugin.getTypeManager().getTypes().forEach(type -> {
+            builder.append("  - ").append(type.getPermission()).append("\n");
+            builder.append("     • ").append(type.getPermission()).append(".fly").append("\n");
+            builder.append("     • ").append(type.getPermission()).append(".hat").append("\n");
+            builder.append("     • ").append(type.getPermission()).append(".mount").append("\n");
+            if (type.getPetData() != null) {
+                builder.append("      → ").append(type.getPermission()).append(".data.*").append("\n");
+                type.getPetData().getItemClasses().forEach(clazz -> {
+                    MenuItem item = getItem(type, clazz);
+                    if (item != null) builder.append("      → ").append(item.getPermission()).append("\n");
+                });
+            }
+        });
+
+        new Logger("Permissions").log(new File(plugin.getDataFolder().toString() + File.separator + "Generated Files" + File.separator), builder.toString());
+
+    }
+}
