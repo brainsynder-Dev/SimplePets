5e9404e8a0d847e3f9cd380fd0fa4b9364fd79f3
Thatsmusic99 <tmtherobotcat@gmail.com>
2018-03-30 07:35:23 +0100

    Updated Rainbow class
    + For new PetData format

--- src/main/java/simplepets/brainsynder/menu/menuItems/Rainbow.java
+++ src/main/java/simplepets/brainsynder/menu/menuItems/Rainbow.java
@@ -1,16 +1,20 @@
 package simplepets.brainsynder.menu.menuItems;
 
 import org.bukkit.Material;
-import simple.brainsynder.api.ItemMaker;
 import simplepets.brainsynder.api.entity.IEntityPet;
 import simplepets.brainsynder.api.entity.IRainbow;
 import simplepets.brainsynder.menu.menuItems.base.MenuItemAbstract;
 import simplepets.brainsynder.pet.PetDefault;
+import simplepets.brainsynder.utils.ItemBuilder;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Rainbow extends MenuItemAbstract {
     //This material is temp... I want to use the SKULL_ITEM with the texture below as the default
     //http://textures.minecraft.net/texture/cffc977cc7e10e564a09638a53bbc4c54c9c8dac7450ba3dfa3c9099d94f5
-    private ItemMaker item = new ItemMaker(Material.REDSTONE_ORE);
+
 
     public Rainbow(PetDefault type, IEntityPet entityPet) {
         super(type, entityPet);
@@ -20,12 +24,20 @@ public class Rainbow extends MenuItemAbstract {
     }
 
     @Override
-    public ItemMaker getItem() {
+    public ItemBuilder getItem() {
+        ItemBuilder item = type.getDataItemByName("rainbow", 0);
         if (entityPet instanceof IRainbow) {
             IRainbow var = (IRainbow) entityPet;
-            item.setName("&6Rainbow: &e" + var.isRainbow());
+            item.withName(String.valueOf(item.toJSON().get("name")).replace("%value%", String.valueOf(var.isRainbow())));
         }
         return item;
+    }
+
+    @Override
+    public List<ItemBuilder> getDefaultItems() {
+        ItemBuilder item = new ItemBuilder(Material.REDSTONE_ORE);
+        item.withName("&6Rainbow: &e%value%");
+        return new ArrayList<>(Collections.singleton(item));
     }
 
     @Override
