7ae5d5e6e016278908be6ed6b506f3c5fe4cf7c9
brainsynder <briansnyder498@gmail.com>
2018-04-01 12:02:42 -0400

    Changed the "Rainbow" mode item to a Skull

--- src/main/java/simplepets/brainsynder/menu/menuItems/Rainbow.java
+++ src/main/java/simplepets/brainsynder/menu/menuItems/Rainbow.java
@@ -12,10 +12,6 @@ import java.util.Collections;
 import java.util.List;
 
 public class Rainbow extends MenuItemAbstract {
-    //This material is temp... I want to use the SKULL_ITEM with the texture below as the default
-    //http://textures.minecraft.net/texture/cffc977cc7e10e564a09638a53bbc4c54c9c8dac7450ba3dfa3c9099d94f5
-
-
     public Rainbow(PetDefault type, IEntityPet entityPet) {
         super(type, entityPet);
     }
@@ -35,8 +31,9 @@ public class Rainbow extends MenuItemAbstract {
 
     @Override
     public List<ItemBuilder> getDefaultItems() {
-        ItemBuilder item = new ItemBuilder(Material.REDSTONE_ORE);
-        item.withName("&6Rainbow: &e%value%");
+        ItemBuilder item = new ItemBuilder(Material.SKULL_ITEM)
+                .withData(3).withName("&6Rainbow: &e%value%")
+                .setTexture("http://textures.minecraft.net/texture/cffc977cc7e10e564a09638a53bbc4c54c9c8dac7450ba3dfa3c9099d94f5");
         return new ArrayList<>(Collections.singleton(item));
     }
 
