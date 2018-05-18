7aecd27de835666ba73dbfde44c0e1afbaaa8976
brainsynder <briansnyder498@gmail.com>
2018-05-18 12:38:38 -0400

    Just added some small 'translations' for the Flying movement

--- src/main/java/simplepets/brainsynder/nms/entities/v1_12_R1/CustomMoveFlying.java
+++ src/main/java/simplepets/brainsynder/nms/entities/v1_12_R1/CustomMoveFlying.java
@@ -6,43 +6,59 @@ import net.minecraft.server.v1_12_R1.GenericAttributes;
 import net.minecraft.server.v1_12_R1.MathHelper;
 
 public class CustomMoveFlying extends ControllerMove {
-    public CustomMoveFlying(EntityInsentient var1) {
-        super(var1);
+    public CustomMoveFlying(EntityInsentient entity) {
+        super(entity);
     }
 
+    /**
+     * NMS translates (Thanks Forge):
+     *
+     * this.a() = this.limitAngle()
+     * MathHelper.c() = MathHelper.atan2()
+     * this.a.o() = this.entity.setMoveVertical()
+     * this.a.n() = this.entity.setMoveForward()
+     * this.a.k() = this.entity.setAIMoveSpeed()
+     *
+     * GenericAttributes.e = SharedMonsterAttributes.FLYING_SPEED
+     */
     public void a() {
+        double posX = this.b;
+        double posY = this.c;
+        double posZ = this.d;
+        double speed = this.e;
+        EntityInsentient entity = this.a;
+
         if (this.h == Operation.MOVE_TO) {
             this.h = Operation.WAIT;
-            this.a.setNoGravity(true);
-            double var1 = this.b - this.a.locX;
-            double var3 = this.c - this.a.locY;
-            double var5 = this.d - this.a.locZ;
+            entity.setNoGravity(true);
+            double var1 = posX - entity.locX;
+            double var3 = posY - entity.locY;
+            double var5 = posZ - entity.locZ;
             double var7 = var1 * var1 + var3 * var3 + var5 * var5;
             if (var7 < 2.500000277905201E-7D) {
-                this.a.o(0.0F);
-                this.a.n(0.0F);
+                entity.o(0.0F);
+                entity.n(0.0F);
                 return;
             }
 
             float var9 = (float) (MathHelper.c(var5, var1) * 57.2957763671875D) - 90.0F;
-            this.a.yaw = this.a(this.a.yaw, var9, 10.0F);
+            entity.yaw = this.a(entity.yaw, var9, 10.0F);
             float var10;
-            if (this.a.onGround) {
-                var10 = (float) (this.e * this.a.getAttributeInstance(GenericAttributes.MOVEMENT_SPEED).getValue());
+            if (entity.onGround) {
+                var10 = (float) (speed * entity.getAttributeInstance(GenericAttributes.MOVEMENT_SPEED).getValue());
             } else {
-                var10 = (float) (this.e * this.a.getAttributeInstance(GenericAttributes.e).getValue());
+                var10 = (float) (speed * entity.getAttributeInstance(GenericAttributes.e).getValue());
             }
 
-            this.a.k(var10);
+            entity.k(var10);
             double var11 = (double) MathHelper.sqrt(var1 * var1 + var5 * var5);
             float var13 = (float) (-(MathHelper.c(var3, var11) * 57.2957763671875D));
-            this.a.pitch = this.a(this.a.pitch, var13, 10.0F);
-            this.a.o(var3 > 0.0D ? var10 : -var10);
+            entity.pitch = this.a(entity.pitch, var13, 10.0F);
+            entity.o(var3 > 0.0D ? var10 : -var10);
         } else {
-            this.a.setNoGravity(false);
-            this.a.o(0.0F);
-            this.a.n(0.0F);
+            entity.setNoGravity(false);
+            entity.o(0.0F);
+            entity.n(0.0F);
         }
-
     }
 }
