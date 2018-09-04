34d5f6c2c50315b7376b583a5e087f5ebeffc642
brainsynder <briansnyder498@gmail.com>
2018-09-03 20:56:37 -0400

    Added 1.13.1 to the 'POM.xml' and 'fetch_dependencies.sh' files

--- fetch_dependencies.sh
+++ fetch_dependencies.sh
@@ -3,4 +3,5 @@ rm libs/craftbukkit-*
 wget https://yivesmirror.com/files/craftbukkit/craftbukkit-1.11.2-R0.1-SNAPSHOT.jar -P libs/
 wget https://yivesmirror.com/files/craftbukkit/craftbukkit-1.12.2-R0.1-SNAPSHOT-b1479.jar -P libs/
 wget http://yivesmirror.com/files/spigot/spigot-1.13-R0.1-SNAPSHOT-b1726.jar -P libs/
+wget http://yivesmirror.com/files/spigot/spigot-1.13.1-R0.1-SNAPSHOT-b1887.jar libs/
 mv libs/craftbukkit-1.12.2-R0.1-SNAPSHOT-b1479.jar libs/craftbukkit-1.12.2-R0.1-SNAPSHOT.jar
