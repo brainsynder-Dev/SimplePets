107a2050763707499afc2c621956b2d403fc9938
brainsynder <briansnyder498@gmail.com>
2019-10-09 13:18:05 -0400

    Fixed Villager pet now working on both 1.14/1.14.4 (Will now work on both)

--- v1_14_R1/src/main/java/simplepets/brainsynder/nms/v1_14_R1/entities/list/EntityVillagerPet.java
+++ v1_14_R1/src/main/java/simplepets/brainsynder/nms/v1_14_R1/entities/list/EntityVillagerPet.java
@@ -2,13 +2,13 @@ package simplepets.brainsynder.nms.v1_14_R1.entities.list;
 
 import net.minecraft.server.v1_14_R1.*;
 import simple.brainsynder.nbt.StorageTagCompound;
-import simple.brainsynder.utils.Reflection;
 import simplepets.brainsynder.PetCore;
 import simplepets.brainsynder.api.Size;
 import simplepets.brainsynder.api.entity.passive.IEntityVillagerPet;
 import simplepets.brainsynder.api.pet.IPet;
 import simplepets.brainsynder.nms.v1_14_R1.entities.AgeableEntityPet;
 import simplepets.brainsynder.nms.v1_14_R1.utils.DataWatcherWrapper;
+import simplepets.brainsynder.nms.v1_14_R1.utils.EntityUtils;
 import simplepets.brainsynder.player.PetOwner;
 import simplepets.brainsynder.wrapper.ProfessionWrapper;
 import simplepets.brainsynder.wrapper.villager.BiomeType;
@@ -37,7 +37,7 @@ public class EntityVillagerPet extends AgeableEntityPet implements IEntityVillag
     @Override
     protected void registerDatawatchers() {
         super.registerDatawatchers();
-        this.datawatcher.register(VILLAGER_DATA, new net.minecraft.server.v1_14_R1.VillagerData(findType("c", "PLAINS"), VillagerProfession.NONE, 1));
+        this.datawatcher.register(VILLAGER_DATA, new net.minecraft.server.v1_14_R1.VillagerData(EntityUtils.getTypeFromBiome(BiomeType.PLAINS), VillagerProfession.NONE, 1));
     }
 
     @Override
@@ -58,113 +58,21 @@ public class EntityVillagerPet extends AgeableEntityPet implements IEntityVillag
         super.applyCompound(object);
     }
 
-    private net.minecraft.server.v1_14_R1.VillagerData getRawData () {
+    private net.minecraft.server.v1_14_R1.VillagerData getRawData() {
         return datawatcher.get(VILLAGER_DATA);
     }
 
     @Override
     public VillagerData getVillagerData() {
-        return  new VillagerData(getBiomeFromType(getRawData().getType()), VillagerType.getVillagerType(getRawData().getProfession().toString()), getRawData().getLevel());
+        net.minecraft.server.v1_14_R1.VillagerData raw = getRawData();
+        return  new VillagerData(EntityUtils.getBiomeFromType(raw.getType()), VillagerType.getVillagerType(raw.getProfession().toString()), raw.getLevel());
     }
 
     @Override
     public void setVillagerData(VillagerData data) {
-        net.minecraft.server.v1_14_R1.VillagerType biome = getTypeFromBiome(data.getBiome());
+        net.minecraft.server.v1_14_R1.VillagerType biome = EntityUtils.getTypeFromBiome(data.getBiome());
 
-        datawatcher.set(VILLAGER_DATA, new net.minecraft.server.v1_14_R1.VillagerData(biome, getProfession(data.getType()), data.getLevel()));
+        datawatcher.set(VILLAGER_DATA, new net.minecraft.server.v1_14_R1.VillagerData(biome, EntityUtils.getProfession(data.getType()), data.getLevel()));
         PetCore.get().getInvLoaders().PET_DATA.update(PetOwner.getPetOwner(getOwner()));
-    }
-
-    private BiomeType getBiomeFromType (net.minecraft.server.v1_14_R1.VillagerType type) {
-        BiomeType biome = BiomeType.PLAINS;
-        if (type == findType("a", "DESERT")) {
-            biome = BiomeType.DESERT;
-        }else if (type == findType("b", "JUNGLE")) {
-            biome = BiomeType.JUNGLE;
-        }else if (type == findType("d", "SAVANNA")) {
-            biome = BiomeType.SAVANNA;
-        }else if (type == findType("e", "SNOW")) {
-            biome = BiomeType.SNOW;
-        }else if (type == findType("f", "SWAMP")) {
-            biome = BiomeType.SWAMP;
-        }else if (type == findType("g", "TAIGA")) {
-            biome = BiomeType.TAIGA;
-        }
-        return biome;
-    }
-    private net.minecraft.server.v1_14_R1.VillagerType getTypeFromBiome (BiomeType type) {
-        net.minecraft.server.v1_14_R1.VillagerType biome = findType("c", "PLAINS");
-        if (type == BiomeType.DESERT) {
-            biome = findType("a", "DESERT");
-        }else if (type == BiomeType.JUNGLE) {
-            biome = findType("b", "JUNGLE");
-        }else if (type == BiomeType.SAVANNA) {
-            biome = findType("d", "SAVANNA");
-        }else if (type == BiomeType.SNOW) {
-            biome = findType("e", "SNOW");
-        }else if (type == BiomeType.SWAMP) {
-            biome = findType("f", "SWAMP");
-        }else if (type == BiomeType.TAIGA) {
-            biome = findType("g", "TAIGA");
-        }
-        return biome;
-    }
-
-    private net.minecraft.server.v1_14_R1.VillagerType findType (String... names) {
-        for (String value : names) {
-            try {
-                return Reflection.getNMSStaticField("VillagerType", value);
-            }catch (Throwable ignored){}
-        }
-        return null;
-    }
-    private net.minecraft.server.v1_14_R1.VillagerProfession getProfession (VillagerType type) {
-
-        net.minecraft.server.v1_14_R1.VillagerProfession profession = VillagerProfession.NONE;
-        switch (type) {
-            case ARMORER:
-                profession = VillagerProfession.ARMORER;
-                break;
-            case BUTCHER:
-                profession = VillagerProfession.BUTCHER;
-                break;
-            case CARTOGRAPHER:
-                profession = VillagerProfession.CARTOGRAPHER;
-                break;
-            case CLERIC:
-                profession = VillagerProfession.CLERIC;
-                break;
-            case FARMER:
-                profession = VillagerProfession.FARMER;
-                break;
-            case FISHERMAN:
-                profession = VillagerProfession.FISHERMAN;
-                break;
-            case FLETCHER:
-                profession = VillagerProfession.FLETCHER;
-                break;
-            case LEATHERWORKER:
-                profession = VillagerProfession.LEATHERWORKER;
-                break;
-            case LIBRARIAN:
-                profession = VillagerProfession.LIBRARIAN;
-                break;
-            case MASON:
-                profession = VillagerProfession.MASON;
-                break;
-            case NITWIT:
-                profession = VillagerProfession.NITWIT;
-                break;
-            case SHEPHERD:
-                profession = VillagerProfession.SHEPHERD;
-                break;
-            case TOOLSMITH:
-                profession = VillagerProfession.TOOLSMITH;
-                break;
-            case WEAPONSMITH:
-                profession = VillagerProfession.WEAPONSMITH;
-                break;
-        }
-        return profession;
     }
 }
--- v1_14_R1/src/main/java/simplepets/brainsynder/nms/v1_14_R1/utils/EntityUtils.java
+++ v1_14_R1/src/main/java/simplepets/brainsynder/nms/v1_14_R1/utils/EntityUtils.java
@@ -1,13 +1,127 @@
 package simplepets.brainsynder.nms.v1_14_R1.utils;
 
-import net.minecraft.server.v1_14_R1.EntityTypes;
-import net.minecraft.server.v1_14_R1.IRegistry;
-import net.minecraft.server.v1_14_R1.MinecraftKey;
+import net.minecraft.server.v1_14_R1.*;
+import simple.brainsynder.utils.Reflection;
 import simplepets.brainsynder.wrapper.EntityWrapper;
+import simplepets.brainsynder.wrapper.villager.BiomeType;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
 
 public class EntityUtils {
+    private static Map<BiomeType, VillagerType> stored = new HashMap<>();
+    private static Map<VillagerType, BiomeType> storedInverted = new HashMap<>();
 
     public static EntityTypes<?> getType (EntityWrapper wrapper) {
         return IRegistry.ENTITY_TYPE.get(new MinecraftKey(wrapper.getTypeName()));
+    }
+
+    public static VillagerType getTypeFromBiome(BiomeType type) {
+        if (stored.isEmpty()) initStores();
+        return stored.get(type);
+    }
+
+    public static BiomeType getBiomeFromType(VillagerType type) {
+        if (storedInverted.isEmpty()) initStores();
+        return storedInverted.get(type);
+    }
+
+    public static VillagerType findType(String... names) {
+        for (String value : names) {
+            Field field = Reflection.getField(VillagerType.class, value);
+            if (field == null) continue;
+
+            Object object = null;
+            try {
+                object = field.get(null);
+            } catch (IllegalAccessException e1) {
+                e1.printStackTrace();
+            }
+            if (object != null)
+                return (VillagerType) object;
+        }
+        return null;
+    }
+
+    private static void initStores () {
+        VillagerType DESERT = findType("a", "DESERT"),
+                JUNGLE = findType("b", "JUNGLE"),
+                PLAINS = findType("c", "PLAINS"),
+                SAVANNA = findType("d", "SAVANNA"),
+                SNOW = findType("e", "SNOW"),
+                SWAMP = findType("f", "SWAMP"),
+                TAIGA = findType("g", "TAIGA");
+
+        if (stored.isEmpty()) {
+            stored.put(BiomeType.DESERT, DESERT);
+            stored.put(BiomeType.JUNGLE, JUNGLE);
+            stored.put(BiomeType.PLAINS, PLAINS);
+            stored.put(BiomeType.SAVANNA, SAVANNA);
+            stored.put(BiomeType.SNOW, SNOW);
+            stored.put(BiomeType.SWAMP, SWAMP);
+            stored.put(BiomeType.TAIGA, TAIGA);
+        }
+
+        if (storedInverted.isEmpty()) {
+            storedInverted.put(DESERT, BiomeType.DESERT);
+            storedInverted.put(JUNGLE, BiomeType.JUNGLE);
+            storedInverted.put(PLAINS, BiomeType.PLAINS);
+            storedInverted.put(SAVANNA, BiomeType.SAVANNA);
+            storedInverted.put(SNOW,BiomeType.SNOW);
+            storedInverted.put(SWAMP,BiomeType.SWAMP);
+            storedInverted.put(TAIGA,BiomeType.TAIGA);
+        }
+    }
+
+
+    public static net.minecraft.server.v1_14_R1.VillagerProfession getProfession(simplepets.brainsynder.wrapper.villager.VillagerType type) {
+
+        net.minecraft.server.v1_14_R1.VillagerProfession profession = VillagerProfession.NONE;
+        switch (type) {
+            case ARMORER:
+                profession = VillagerProfession.ARMORER;
+                break;
+            case BUTCHER:
+                profession = VillagerProfession.BUTCHER;
+                break;
+            case CARTOGRAPHER:
+                profession = VillagerProfession.CARTOGRAPHER;
+                break;
+            case CLERIC:
+                profession = VillagerProfession.CLERIC;
+                break;
+            case FARMER:
+                profession = VillagerProfession.FARMER;
+                break;
+            case FISHERMAN:
+                profession = VillagerProfession.FISHERMAN;
+                break;
+            case FLETCHER:
+                profession = VillagerProfession.FLETCHER;
+                break;
+            case LEATHERWORKER:
+                profession = VillagerProfession.LEATHERWORKER;
+                break;
+            case LIBRARIAN:
+                profession = VillagerProfession.LIBRARIAN;
+                break;
+            case MASON:
+                profession = VillagerProfession.MASON;
+                break;
+            case NITWIT:
+                profession = VillagerProfession.NITWIT;
+                break;
+            case SHEPHERD:
+                profession = VillagerProfession.SHEPHERD;
+                break;
+            case TOOLSMITH:
+                profession = VillagerProfession.TOOLSMITH;
+                break;
+            case WEAPONSMITH:
+                profession = VillagerProfession.WEAPONSMITH;
+                break;
+        }
+        return profession;
     }
 }
