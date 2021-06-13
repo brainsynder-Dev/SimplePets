657f70445ab7980b46bdb7c774efd6e101e297ba
brainsynder <briansnyder498@gmail.com>
2021-06-13 00:20:09 -0400

    Fixed AxolotlPlayDeadData missing "true" in the name

--- API/src/main/java/simplepets/brainsynder/api/pet/data/AxolotlPlayDeadData.java
+++ API/src/main/java/simplepets/brainsynder/api/pet/data/AxolotlPlayDeadData.java
@@ -14,7 +14,7 @@ public class AxolotlPlayDeadData extends PetData<IEntityAxolotlPet> {
                 .withName("&#c8c8c8{name}: &cfalse")
                 .setTexture(AxolotlVariant.LUCY.getTexture()));
         addDefaultItem("true", new ItemBuilder(Material.PLAYER_HEAD)
-                .withName("&#c8c8c8{name}: &a")
+                .withName("&#c8c8c8{name}: &atrue")
                 .setTexture("http://textures.minecraft.net/texture/88fd654d856bde8b69f0c3567d28fafe94e71eae10d32ea59ee23e9bd64b41b0"));
 
     }
