63cc1b30bb40bee286d1d0b77d678f9940618327
brainsynder <briansnyder498@gmail.com>
2021-06-13 11:59:50 -0400

    This has to work right?

--- API/src/main/java/simplepets/brainsynder/api/event/inventory/PetTypeStorage.java
+++ API/src/main/java/simplepets/brainsynder/api/event/inventory/PetTypeStorage.java
@@ -4,25 +4,23 @@ import org.bukkit.inventory.ItemStack;
 import simplepets.brainsynder.api.pet.PetType;
 import simplepets.brainsynder.api.plugin.SimplePets;
 
-import javax.annotation.Nonnull;
-
 public class PetTypeStorage {
     private final PetType type;
     private ItemStack item;
 
-    public PetTypeStorage(@Nonnull PetType type) {
+    public PetTypeStorage(PetType type) {
         this.type = type;
         SimplePets.getPetConfigManager().getPetConfig(type).ifPresent(config -> {
             PetTypeStorage.this.item = config.getBuilder().build();
         });
     }
 
-    public PetTypeStorage(@Nonnull PetType type, ItemStack item) {
+    public PetTypeStorage(PetType type, ItemStack item) {
         this.type = type;
         this.item = item;
     }
 
-    public boolean isSimilar(@Nonnull PetTypeStorage storage) {
+    public boolean isSimilar(PetTypeStorage storage) {
         return (type == storage.type) && (item.isSimilar(storage.item));
     }
 
