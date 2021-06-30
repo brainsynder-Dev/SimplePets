b41b982fccd10a6b950f88d672ba605405ca174c
brainsynder <briansnyder498@gmail.com>
2021-06-30 13:45:12 -0400

    Added comment about horse riding

--- v1_17_R1/src/main/java/simplepets/brainsynder/versions/v1_17_R1/entity/EntityPet.java
+++ v1_17_R1/src/main/java/simplepets/brainsynder/versions/v1_17_R1/entity/EntityPet.java
@@ -362,6 +362,7 @@ public abstract class EntityPet extends Mob implements IEntityPet {
         super.travel(new Vec3(strafe, vertical, forward));
 
         // This bit of code at least keeps the special riding mobs in the correct location
+        // Horse riding is still bugged but at least its not in the wrong spot
         if (this instanceof ISpecialRiding) {
             ClientboundMoveVehiclePacket packet = new ClientboundMoveVehiclePacket(this);
             passenger.connection.send(packet);
