2d0c09cd3babeb340d6ed728c76c2459164f2d38
brainsynder <briansnyder498@gmail.com>
2021-06-30 11:42:01 -0400

    Added an optional parameter to the `/pet gui` command Will allow other players/console to open the gui for other players

--- MAIN/src/main/java/simplepets/brainsynder/commands/list/GUICommand.java
+++ MAIN/src/main/java/simplepets/brainsynder/commands/list/GUICommand.java
@@ -1,6 +1,7 @@
 package simplepets.brainsynder.commands.list;
 
 import lib.brainsynder.commands.annotations.ICommand;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import simplepets.brainsynder.PetCore;
@@ -11,16 +12,29 @@ import simplepets.brainsynder.menu.inventory.SelectionMenu;
 
 @ICommand(
         name = "gui",
+        usage = "[player]",
         description = "Opens the pet gui"
 )
-@Permission(permission = "gui", defaultAllow = true)
+@Permission(permission = "gui", defaultAllow = true, additionalPermissions = {"other"})
 public class GUICommand extends PetSubCommand {
     public GUICommand(PetCore plugin) {
         super(plugin);
     }
 
     @Override
-    public void run(CommandSender sender) {
+    public void run(CommandSender sender, String[] args) {
+        if ((args.length > 0) && sender.hasPermission(getPermission("other"))) {
+            String selector = args[0];
+            Player target = Bukkit.getPlayerExact(selector);
+            if (target != null) {
+
+                // Will open the selection gui for the selected player
+                SimplePets.getUserManager().getPetUser(target).ifPresent(user -> {
+                    SimplePets.getGUIHandler().getInventory(SelectionMenu.class).ifPresent(selectionMenu -> selectionMenu.open(user));
+                });
+                return;
+            }
+        }
         SimplePets.getUserManager().getPetUser((Player) sender).ifPresent(user -> {
             SimplePets.getGUIHandler().getInventory(SelectionMenu.class).ifPresent(selectionMenu -> selectionMenu.open(user));
         });
