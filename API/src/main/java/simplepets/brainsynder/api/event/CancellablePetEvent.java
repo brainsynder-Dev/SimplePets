65e9bfee7a5164d92d6c28bbc28328a82fc36735
brainsynder <briansnyder498@gmail.com>
2021-07-19 01:30:14 -0400

    CancellablePetEvent can now have a reason as to why it was canceled

--- API/src/main/java/simplepets/brainsynder/api/event/CancellablePetEvent.java
+++ API/src/main/java/simplepets/brainsynder/api/event/CancellablePetEvent.java
@@ -4,8 +4,23 @@ import org.bukkit.event.Cancellable;
 
 public abstract class CancellablePetEvent extends SimplePetEvent implements Cancellable {
     private boolean cancelled = false;
+    private String reason = null;
 
-    public boolean isCancelled() {return this.cancelled;}
 
-    public void setCancelled(boolean cancelled) {this.cancelled = cancelled; }
+    public boolean isCancelled() {
+        return this.cancelled;
+    }
+
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
+    }
+
+    public void setCancelled(boolean cancelled, String reason) {
+        this.cancelled = cancelled;
+        this.reason = reason;
+    }
+
+    public String getReason() {
+        return reason;
+    }
 }
