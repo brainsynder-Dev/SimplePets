4dc7f2018158737d68f51a2a2b517e71f248d25d
Traks <traksag@protonmail.com>
2021-07-22 12:44:16 +0200

    Add tropical fish data from v4

---
+++ API/src/main/java/simplepets/brainsynder/api/pet/data/TropicalPatternColorData.java
@@ -0,0 +1,72 @@
+package simplepets.brainsynder.api.pet.data;
+
+import java.util.Optional;
+import lib.brainsynder.item.ItemBuilder;
+import lib.brainsynder.nms.DataConverter;
+import lib.brainsynder.utils.DyeColorWrapper;
+import org.apache.commons.lang.WordUtils;
+import simplepets.brainsynder.api.Namespace;
+import simplepets.brainsynder.api.entity.passive.IEntityTropicalFishPet;
+import simplepets.brainsynder.api.pet.PetData;
+
+@Namespace(namespace = "pattern_color")
+public class TropicalPatternColorData extends PetData<IEntityTropicalFishPet> {
+    public TropicalPatternColorData() {
+        for (DyeColorWrapper color : DyeColorWrapper.values()) {
+            addDefaultItem(color.name(), DataConverter.getColoredMaterial(DataConverter.MaterialType.STAINED_CLAY, color)
+                    .withName(" ")
+                    .addLore(
+                            "&#c8c8c8Previous: {previousColor}{previousName}",
+                            "&#c8c8c8Current: {currentColor}{currentName}",
+                            "&#c8c8c8Next: {nextColor}{nextName}"));
+        }
+    }
+
+    @Override
+    public Object getDefaultValue() {
+        return DyeColorWrapper.WHITE;
+    }
+
+    @Override
+    public Optional<ItemBuilder> getItem(IEntityTropicalFishPet entity) {
+        Optional<ItemBuilder> optional = super.getItem(entity);
+        if (optional.isPresent()) {
+            // We have to do this to replace the placholders if there is any
+
+            DyeColorWrapper previous = DyeColorWrapper.getPrevious(entity.getPatternColor());
+            DyeColorWrapper next = DyeColorWrapper.getNext(entity.getPatternColor());
+
+            ItemBuilder builder = optional.get();
+            builder.replaceInLore("{previousColor}", previous.getChatColor())
+                    .replaceInLore("{currentColor}", entity.getPatternColor().getChatColor())
+                    .replaceInLore("{nextColor}", next.getChatColor())
+                    .replaceInLore("{previousName}", WordUtils.capitalize(previous.name().toLowerCase().replace("_", " ")))
+                    .replaceInLore("{currentName}", WordUtils.capitalize(entity.getPatternColor().name().toLowerCase().replace("_", " ")))
+                    .replaceInLore("{nextName}", WordUtils.capitalize(next.name().toLowerCase().replace("_", " ")));
+
+            builder.replaceInName("{previousColor}", previous.getChatColor())
+                    .replaceInName("{currentColor}", entity.getPatternColor().getChatColor())
+                    .replaceInName("{nextColor}", next.getChatColor())
+                    .replaceInName("{previousName}", WordUtils.capitalize(previous.name().toLowerCase().replace("_", " ")))
+                    .replaceInName("{currentName}", WordUtils.capitalize(entity.getPatternColor().name().toLowerCase().replace("_", " ")))
+                    .replaceInName("{nextName}", WordUtils.capitalize(next.name().toLowerCase().replace("_", " ")));
+            return Optional.of(builder);
+        }
+        return optional;
+    }
+
+    @Override
+    public void onLeftClick(IEntityTropicalFishPet entity) {
+        entity.setPatternColor(DyeColorWrapper.getNext(entity.getPatternColor()));
+    }
+
+    @Override
+    public void onRightClick(IEntityTropicalFishPet entity) {
+        entity.setPatternColor(DyeColorWrapper.getPrevious(entity.getPatternColor()));
+    }
+
+    @Override
+    public Object value(IEntityTropicalFishPet entity) {
+        return entity.getPatternColor();
+    }
+}
---
+++ API/src/main/java/simplepets/brainsynder/api/pet/data/TropicalPatternData.java
@@ -0,0 +1,70 @@
+package simplepets.brainsynder.api.pet.data;
+
+import java.util.Optional;
+import lib.brainsynder.item.ItemBuilder;
+import lib.brainsynder.utils.DyeColorWrapper;
+import org.apache.commons.lang.WordUtils;
+import org.bukkit.Material;
+import simplepets.brainsynder.api.Namespace;
+import simplepets.brainsynder.api.entity.passive.IEntityTropicalFishPet;
+import simplepets.brainsynder.api.pet.PetData;
+import simplepets.brainsynder.api.wrappers.TropicalPattern;
+
+@Namespace(namespace = "pattern")
+public class TropicalPatternData extends PetData<IEntityTropicalFishPet> {
+    public TropicalPatternData() {
+        for (TropicalPattern pattern : TropicalPattern.values()) {
+            addDefaultItem(pattern.name(), new ItemBuilder(Material.PLAYER_HEAD)
+                    .setTexture("http://textures.minecraft.net/texture/36d149e4d499929672e2768949e6477959c21e65254613b327b538df1e4df")
+                    .withName("&#c8c8c8{name}: &a" + pattern.name()));
+        }
+    }
+
+    @Override
+    public Object getDefaultValue() {
+        return TropicalPattern.KOB;
+    }
+
+    @Override
+    public Optional<ItemBuilder> getItem(IEntityTropicalFishPet entity) {
+        Optional<ItemBuilder> optional = super.getItem(entity);
+        if (optional.isPresent()) {
+            // We have to do this to replace the placholders if there is any
+
+            DyeColorWrapper previous = DyeColorWrapper.getPrevious(entity.getPatternColor());
+            DyeColorWrapper next = DyeColorWrapper.getNext(entity.getPatternColor());
+
+            ItemBuilder builder = optional.get();
+            builder.replaceInLore("{previousColor}", previous.getChatColor())
+                    .replaceInLore("{currentColor}", entity.getPatternColor().getChatColor())
+                    .replaceInLore("{nextColor}", next.getChatColor())
+                    .replaceInLore("{previousName}", WordUtils.capitalize(previous.name().toLowerCase().replace("_", " ")))
+                    .replaceInLore("{currentName}", WordUtils.capitalize(entity.getPatternColor().name().toLowerCase().replace("_", " ")))
+                    .replaceInLore("{nextName}", WordUtils.capitalize(next.name().toLowerCase().replace("_", " ")));
+
+            builder.replaceInName("{previousColor}", previous.getChatColor())
+                    .replaceInName("{currentColor}", entity.getPatternColor().getChatColor())
+                    .replaceInName("{nextColor}", next.getChatColor())
+                    .replaceInName("{previousName}", WordUtils.capitalize(previous.name().toLowerCase().replace("_", " ")))
+                    .replaceInName("{currentName}", WordUtils.capitalize(entity.getPatternColor().name().toLowerCase().replace("_", " ")))
+                    .replaceInName("{nextName}", WordUtils.capitalize(next.name().toLowerCase().replace("_", " ")));
+            return Optional.of(builder);
+        }
+        return optional;
+    }
+
+    @Override
+    public void onLeftClick(IEntityTropicalFishPet entity) {
+        entity.setPattern(TropicalPattern.getNext(entity.getPattern()));
+    }
+
+    @Override
+    public void onRightClick(IEntityTropicalFishPet entity) {
+        entity.setPattern(TropicalPattern.getPrevious(entity.getPattern()));
+    }
+
+    @Override
+    public Object value(IEntityTropicalFishPet entity) {
+        return entity.getPattern();
+    }
+}
