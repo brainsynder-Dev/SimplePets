ba9bf3930e974b10d8fbeb7c92a3a69247b30d1e
Thatsmusic99 <25277367+Thatsmusic99@users.noreply.github.com>
2021-10-02 15:12:11 +0100

    Disable damage when the pet is flyable

--- MAIN/src/main/java/simplepets/brainsynder/listeners/DamageListener.java
+++ MAIN/src/main/java/simplepets/brainsynder/listeners/DamageListener.java
@@ -5,7 +5,12 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import simplepets.brainsynder.api.entity.IEntityPet;
+import simplepets.brainsynder.api.pet.IPetConfig;
 import simplepets.brainsynder.api.plugin.SimplePets;
+import simplepets.brainsynder.api.user.PetUser;
+
+import java.util.Optional;
 
 public class DamageListener implements Listener {
 
@@ -16,6 +21,13 @@ public class DamageListener implements Listener {
         Player player = (Player) event.getEntity();
         if (!player.isInsideVehicle()) return;
         Entity entity = player.getVehicle();
-        if (entity != null && SimplePets.isPetEntity(entity)) event.setCancelled(true);
+        if (entity == null || !SimplePets.isPetEntity(entity)) return;
+        Optional<Object> petHandle = SimplePets.getSpawnUtil().getHandle(entity);
+        if (!petHandle.isPresent()) return;
+        if (!(petHandle.get() instanceof IEntityPet)) return;
+        IEntityPet pet = (IEntityPet) petHandle.get();
+        Optional<IPetConfig> config = SimplePets.getPetConfigManager().getPetConfig(pet.getPetType());
+        if (!config.isPresent()) return;
+        if (config.get().canFly(player)) event.setCancelled(true);
     }
 }
