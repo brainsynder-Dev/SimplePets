22a8bf9972c08f595eafc30f2e713cf9ba2b07c1
brainsynder <briansnyder498@gmail.com>
2021-11-10 01:23:53 -0500

    Re-Structured the DamageListener.java which should fix the issue with pets allowing their owner to take fall damage.

--- MAIN/src/main/java/simplepets/brainsynder/listeners/DamageListener.java
+++ MAIN/src/main/java/simplepets/brainsynder/listeners/DamageListener.java
@@ -5,29 +5,38 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import simplepets.brainsynder.PetCore;
 import simplepets.brainsynder.api.entity.IEntityPet;
-import simplepets.brainsynder.api.pet.IPetConfig;
 import simplepets.brainsynder.api.plugin.SimplePets;
-import simplepets.brainsynder.api.user.PetUser;
-
-import java.util.Optional;
+import simplepets.brainsynder.files.Config;
 
 public class DamageListener implements Listener {
 
     @EventHandler(ignoreCancelled = true)
     public void onPlayerFall(EntityDamageEvent event) {
         if (!(event.getEntity() instanceof Player)) return;
-        if (!event.getCause().equals(EntityDamageEvent.DamageCause.FALL)) return;
+        if (!event.getCause().equals(EntityDamageEvent.DamageCause.FALL)) return; // Only listen if the cause is Fall Damage
         Player player = (Player) event.getEntity();
-        if (!player.isInsideVehicle()) return;
-        Entity entity = player.getVehicle();
-        if (entity == null || !SimplePets.isPetEntity(entity)) return;
-        Optional<Object> petHandle = SimplePets.getSpawnUtil().getHandle(entity);
-        if (!petHandle.isPresent()) return;
-        if (!(petHandle.get() instanceof IEntityPet)) return;
-        IEntityPet pet = (IEntityPet) petHandle.get();
-        Optional<IPetConfig> config = SimplePets.getPetConfigManager().getPetConfig(pet.getPetType());
-        if (!config.isPresent()) return;
-        if (config.get().canFly(player)) event.setCancelled(true);
+
+        Entity vehicle = player.getVehicle();
+        if (vehicle == null) return;
+        if (!SimplePets.isPetEntity(vehicle)) return; // The vehicle is not a Pet
+
+        SimplePets.getSpawnUtil().getHandle(vehicle).ifPresent(obj -> {
+            IEntityPet pet = (IEntityPet) obj;
+            SimplePets.getPetConfigManager().getPetConfig(pet.getPetType()).ifPresent(config -> { // Fetch the pets config
+                Config configuration = PetCore.getInstance().getConfiguration();
+
+                if (config.canFly(player)) { // Checks if a pet is a flyable pet
+                    if (!configuration.getBoolean("PetToggles.FallDamage.Flyable-Pets", true)) return;
+                    event.setCancelled(true);
+                    return;
+                }
+
+                // Pet is not flyable...
+                if (!configuration.getBoolean("PetToggles.FallDamage.Non-Flyable-Pets", true)) return;
+                event.setCancelled(true);
+            });
+        });
     }
 }
