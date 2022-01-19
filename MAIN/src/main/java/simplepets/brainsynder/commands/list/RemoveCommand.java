2ccb73c52aab7ff6831c2089039880ca4b6d83a9
brainsynder <briansnyder498@gmail.com>
2022-01-19 11:43:03 -0500

    Added player targeting to '/pet remove' https://github.com/brainsynder-Dev/SimplePets/issues/137

--- MAIN/src/main/java/simplepets/brainsynder/commands/list/RemoveCommand.java
+++ MAIN/src/main/java/simplepets/brainsynder/commands/list/RemoveCommand.java
@@ -1,6 +1,8 @@
 package simplepets.brainsynder.commands.list;
 
 import lib.brainsynder.commands.annotations.ICommand;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import simplepets.brainsynder.PetCore;
@@ -15,7 +17,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 @ICommand(
         name = "remove",
-        usage = "[type]",
+        usage = "[player] [type]",
         description = "Remove your pet or another players"
 )
 @Permission(permission = "remove", defaultAllow = true, additionalPermissions = {"other"})
@@ -26,17 +28,15 @@ public class RemoveCommand extends PetSubCommand {
 
     @Override
     public void run(CommandSender sender, String[] args) {
-        if (!(sender instanceof Player)) {
-            //TODO: not player message
-            return;
-        }
-
-        Player player = (Player) sender;
-
         if (args.length == 0) {
+            if (!(sender instanceof Player)) {
+                sendUsage(sender);
+                return;
+            }
+
             AtomicInteger integer = new AtomicInteger(0);
 
-            getPlugin().getUserManager().getPetUser(player).ifPresent(user -> {
+            getPlugin().getUserManager().getPetUser((Player)sender).ifPresent(user -> {
                 for (PetType type : PetType.values())
                 if (user.removePet(type)) integer.incrementAndGet();
             });
@@ -44,17 +44,52 @@ public class RemoveCommand extends PetSubCommand {
             sender.sendMessage(MessageFile.getTranslation(MessageOption.REMOVED_ALL_PETS).replace("{count}", String.valueOf(integer.get())));
             return;
         }
+        AtomicInteger index = new AtomicInteger(0);
 
+        Player target = null;
+        if (isUsername(args[index.get()]) && sender.hasPermission(getPermission("other"))) {
+            Player selected = Bukkit.getPlayerExact(args[index.get()]);
+            if (selected != null) {
+                target = selected;
+                if (!sender.hasPermission(getPermission("other"))) {
+                    sender.sendMessage(MessageFile.getTranslation(MessageOption.NO_PERMISSION));
+                    return;
+                }
+                index.getAndIncrement();
+            }
+        }
 
-        Optional<PetType> petType = PetType.getPetType(args[0]);
+        if (target == null) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage(ChatColor.RED+"You must be a player to run this command for yourself.");
+            }else{
+                target = (Player) sender;
+            }
+        }
+
+        Player finalTarget = target;
+        if (args.length == index.get()) {
+            AtomicInteger integer = new AtomicInteger(0);
+
+            getPlugin().getUserManager().getPetUser(finalTarget).ifPresent(user -> {
+                for (PetType type : PetType.values())
+                    if (user.removePet(type)) integer.incrementAndGet();
+            });
+
+            sender.sendMessage(MessageFile.getTranslation(MessageOption.REMOVED_ALL_PETS).replace("{count}", String.valueOf(integer.get())));
+
+            return;
+        }
+
+        Optional<PetType> petType = PetType.getPetType(args[index.get()]);
         if (!petType.isPresent()) {
-            sender.sendMessage(MessageFile.getTranslation(MessageOption.INVALID_PET_TYPE).replace("{arg}", args[0]));
+            sender.sendMessage(MessageFile.getTranslation(MessageOption.INVALID_PET_TYPE).replace("{arg}", args[index.get()]));
             return;
         }
 
         PetType type = petType.get();
 
-        getPlugin().getUserManager().getPetUser(((Player)sender).getUniqueId()).ifPresent(user -> {
+        getPlugin().getUserManager().getPetUser(finalTarget).ifPresent(user -> {
             if (!user.removePet(type)) {
                 sender.sendMessage(MessageFile.getTranslation(MessageOption.REMOVED_PET).replace("{type}", type.getName()));
                 return;
