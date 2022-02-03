702c8bb44a0033ccdd7e434f207e2d702545878d
brainsynder <briansnyder498@gmail.com>
2022-02-02 23:39:15 -0500

    Added `/pet addon update` subcommand

--- MAIN/src/main/java/simplepets/brainsynder/commands/list/AddonCommand.java
+++ MAIN/src/main/java/simplepets/brainsynder/commands/list/AddonCommand.java
@@ -1,6 +1,9 @@
 package simplepets.brainsynder.commands.list;
 
 import lib.brainsynder.commands.annotations.ICommand;
+import lib.brainsynder.json.Json;
+import lib.brainsynder.json.JsonObject;
+import lib.brainsynder.web.WebConnector;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -19,10 +22,10 @@ import java.util.List;
 
 @ICommand(
         name = "addon",
-        usage = "[reload]",
+        usage = "[reload|update] [addon]",
         description = "Opens a GUI to download/toggle addons for the plugin"
 )
-@Permission(permission = "addon", adminCommand = true, additionalPermissions = {"reload"})
+@Permission(permission = "addon", adminCommand = true, additionalPermissions = {"reload", "update"})
 public class AddonCommand extends PetSubCommand {
 
     public AddonCommand(PetCore plugin) {
@@ -32,13 +35,56 @@ public class AddonCommand extends PetSubCommand {
     @Override
     public List<String> handleCompletions(List<String> completions, CommandSender sender, int index, String[] args) {
         if (!canExecute(sender)) return super.handleCompletions(completions, sender, index, args);
-        if ((index == 1) && sender.hasPermission(getPermission("reload"))) completions.add("reload");
+        if ((index == 1)) {
+            if (sender.hasPermission(getPermission("reload"))) completions.add("reload");
+            if (sender.hasPermission(getPermission("update"))) completions.add("update");
+        }
+
+        if (index == 2) {
+            if (args[0].equalsIgnoreCase("update")) {
+                PetCore.getInstance().getAddonManager().getLoadedAddons().forEach(petAddon -> {
+                    completions.add(petAddon.getNamespace().namespace());
+                });
+            }
+        }
         return super.handleCompletions(completions, sender, index, args);
     }
 
     @Override
     public void run(CommandSender sender, String[] args) {
         if (args.length > 0) {
+            if (args[0].equalsIgnoreCase("update") && sender.hasPermission(getPermission("update"))) {
+                AddonManager manager = PetCore.getInstance().getAddonManager();
+
+                if (args.length == 1) {
+                    sendUsage(sender);
+                    return;
+                }
+                String target = args[1];
+                manager.fetchAddon(target).ifPresent(petAddon -> {
+                    String name = petAddon.getNamespace().namespace();
+
+                    WebConnector.getInputStreamString("https://pluginwiki.us/addons/addons.json", getPlugin(), result -> {
+                        JsonObject json = (JsonObject) Json.parse(result);
+                        if (!json.names().contains(name)) {
+                            sender.sendMessage(MessageFile.getTranslation(MessageOption.PREFIX)+" §c"+name+" is not in the addon database: https://pluginwiki.us/addons/");
+                            return;
+                        }
+
+                        String url = ((JsonObject)json.get(name)).getString("url", null);
+                        if (url == null) {
+                            sender.sendMessage(MessageFile.getTranslation(MessageOption.PREFIX)+" §c"+name+" seems to be missing the download URL (Contact brainsynder)");
+                            return;
+                        }
+
+                        manager.update(petAddon, url, () -> {
+                            sender.sendMessage(MessageFile.getTranslation(MessageOption.PREFIX)+" §7"+name+" has been successfully updated!");
+                        });
+                    });
+                });
+
+                return;
+            }
             if (args[0].equalsIgnoreCase("reload") && sender.hasPermission(getPermission("reload"))) {
                 AddonManager manager = PetCore.getInstance().getAddonManager();
                 manager.cleanup();
