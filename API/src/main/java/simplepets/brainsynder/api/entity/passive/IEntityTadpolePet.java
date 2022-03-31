a2c86ba4c211c83540368b7119d4f37687403180
brainsynder <briansnyder498@gmail.com>
2022-03-31 14:01:13 -0400

    forgot to add the @SupportedVersion annotation to the tadpole

--- API/src/main/java/simplepets/brainsynder/api/entity/passive/IEntityTadpolePet.java
+++ API/src/main/java/simplepets/brainsynder/api/entity/passive/IEntityTadpolePet.java
@@ -1,9 +1,12 @@
 package simplepets.brainsynder.api.entity.passive;
 
+import lib.brainsynder.ServerVersion;
+import lib.brainsynder.SupportedVersion;
 import simplepets.brainsynder.api.entity.misc.EntityPetType;
 import simplepets.brainsynder.api.entity.misc.IEntityFishPet;
 import simplepets.brainsynder.api.pet.PetType;
 
+@SupportedVersion(version = ServerVersion.v1_19)
 @EntityPetType(petType = PetType.TADPOLE)
 public interface IEntityTadpolePet extends IEntityFishPet {
 }
