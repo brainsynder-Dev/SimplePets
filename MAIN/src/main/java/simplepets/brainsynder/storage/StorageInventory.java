4d55f5349159869dc2135c57ab2119d6b6baf39d
brainsynder <briansnyder498@gmail.com>
2022-06-03 22:29:52 -0400

    More progress with the Inventory storage code

--- MAIN/src/main/java/simplepets/brainsynder/storage/StorageInventory.java
+++ MAIN/src/main/java/simplepets/brainsynder/storage/StorageInventory.java
@@ -1,29 +1,61 @@
 package simplepets.brainsynder.storage;
 
 import lib.brainsynder.nbt.StorageTagCompound;
+import lib.brainsynder.nbt.StorageTagList;
+import lib.brainsynder.nbt.StorageTagTools;
+import lib.brainsynder.utils.Colorize;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.StringUtil;
 import simplepets.brainsynder.api.pet.PetType;
 import simplepets.brainsynder.api.plugin.config.ConfigOption;
+import simplepets.brainsynder.api.user.PetUser;
 import simplepets.brainsynder.menu.inventory.holders.StorageHolder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class StorageInventory {
     private StorageTagCompound rawCompound;
-    private Inventory inventory;
+    private Map<Integer, ItemStack> items;
 
     public static StorageInventory fromCompound (StorageTagCompound compound) {
         StorageInventory storageInventory = new StorageInventory();
-        storageInventory.inventory = Bukkit.createInventory(
-                new StorageHolder(),
-                getInventorySize(compound.getEnum("type", PetType.class)),
-                ""
-        );
+
+        storageInventory.rawCompound = compound;
+        storageInventory.items = new HashMap<>();
+
+        StorageTagList itemList = (StorageTagList) compound.getTag("items");
+        itemList.getList().forEach(base -> {
+            StorageTagCompound comp = (StorageTagCompound) base;
+
+            storageInventory.items.put(comp.getInteger("slot"), StorageTagTools.toItemStack(comp.getCompoundTag("item")));
+        });
         return storageInventory;
     }
 
-    public static int getInventorySize (PetType type) {
+    public void openInventory (Player player, PetUser user, PetType type) {
+        Inventory inventory = Bukkit.createInventory(new StorageHolder(), getInventorySize(type), getInventoryTitle(user, type));
+        items.forEach(inventory::setItem);
+        player.openInventory(inventory);
+    }
+
+    public StorageTagCompound toCompound () {
+        StorageTagCompound compound = new StorageTagCompound();
+        return compound;
+    }
+
+    private String getInventoryTitle (PetUser user, PetType type) {
+        return Colorize.translateBungeeHex(ConfigOption.INSTANCE.PET_STORAGE_TITLE.getValue()
+                .replace("{player}", user.getOwnerName())
+                .replace("{type}", type.getName())
+        );
+    }
+
+    private int getInventorySize (PetType type) {
         for (String line : ConfigOption.INSTANCE.PET_STORAGE_LIMIT.getValue()) {
             if (!StringUtil.startsWithIgnoreCase(line, type.getName())) continue;
             if (!line.contains("-")) {
