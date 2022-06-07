bae3f5944a65853bccae33aaa3af680254159819
brainsynder <briansnyder498@gmail.com>
2022-06-07 15:03:44 -0400

    Fixed CatEntity code

--- Version_1.19/src/main/java/simplepets/brainsynder/nms/entity/list/EntityCatPet.java
+++ Version_1.19/src/main/java/simplepets/brainsynder/nms/entity/list/EntityCatPet.java
@@ -2,10 +2,12 @@ package simplepets.brainsynder.nms.entity.list;
 
 import lib.brainsynder.nbt.StorageTagCompound;
 import lib.brainsynder.utils.DyeColorWrapper;
+import net.minecraft.core.Registry;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.animal.CatVariant;
 import simplepets.brainsynder.api.entity.passive.IEntityCatPet;
 import simplepets.brainsynder.api.pet.PetType;
 import simplepets.brainsynder.api.user.PetUser;
@@ -13,10 +15,10 @@ import simplepets.brainsynder.api.wrappers.CatType;
 import simplepets.brainsynder.nms.entity.EntityTameablePet;
 
 /**
- * NMS: {@link net.minecraft.server.v1_16_R3.EntityCat}
+ * NMS: {@link net.minecraft.world.entity.animal.Cat}
  */
 public class EntityCatPet extends EntityTameablePet implements IEntityCatPet {
-    private static final EntityDataAccessor<Integer> TYPE; // This is no longer an int, it is a CatVariant
+    private static final EntityDataAccessor<CatVariant> TYPE;
     private static final EntityDataAccessor<Boolean> SLEEPING_WITH_OWNER;
     private static final EntityDataAccessor<Boolean> HEAD_UP;
     private static final EntityDataAccessor<Integer> COLLAR_COLOR;
@@ -28,7 +30,7 @@ public class EntityCatPet extends EntityTameablePet implements IEntityCatPet {
     @Override
     protected void registerDatawatchers() {
         super.registerDatawatchers();
-        entityData.define(TYPE, CatType.TABBY.ordinal());
+        entityData.define(TYPE, CatVariant.TABBY);
         entityData.define(SLEEPING_WITH_OWNER, false);
         entityData.define(HEAD_UP, false);
         entityData.define(COLLAR_COLOR, DyeColorWrapper.WHITE.getWoolData());
@@ -55,12 +57,12 @@ public class EntityCatPet extends EntityTameablePet implements IEntityCatPet {
 
     @Override
     public CatType getCatType() {
-        return CatType.getByID(entityData.get(TYPE));
+        return CatType.getByID(Registry.CAT_VARIANT.getId(entityData.get(TYPE)));
     }
 
     @Override
     public void setCatType(CatType type) {
-        entityData.set(TYPE, type.ordinal());
+        entityData.set(TYPE, Registry.CAT_VARIANT.byId(type.ordinal()));
     }
 
     @Override
@@ -94,7 +96,7 @@ public class EntityCatPet extends EntityTameablePet implements IEntityCatPet {
     }
 
     static {
-        TYPE = SynchedEntityData.defineId(EntityCatPet.class, EntityDataSerializers.INT); // As per 1.19    INT -> CAT_VARIANT
+        TYPE = SynchedEntityData.defineId(EntityCatPet.class, EntityDataSerializers.CAT_VARIANT);
         SLEEPING_WITH_OWNER = SynchedEntityData.defineId(EntityCatPet.class, EntityDataSerializers.BOOLEAN);
         HEAD_UP = SynchedEntityData.defineId(EntityCatPet.class, EntityDataSerializers.BOOLEAN);
         COLLAR_COLOR = SynchedEntityData.defineId(EntityCatPet.class, EntityDataSerializers.INT);
