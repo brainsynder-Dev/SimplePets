0ca89e69839ada241c666f9ed2030a874aeb8b8c
brainsynder <briansnyder498@gmail.com>
2022-06-09 01:06:14 -0400

    Code additions for the allay

--- Version_1.19/src/main/java/simplepets/brainsynder/nms/entity/list/EntityAllayPet.java
+++ Version_1.19/src/main/java/simplepets/brainsynder/nms/entity/list/EntityAllayPet.java
@@ -2,9 +2,9 @@ package simplepets.brainsynder.nms.entity.list;
 
 import lib.brainsynder.ServerVersion;
 import lib.brainsynder.SupportedVersion;
-import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MoverType;
+import net.minecraft.world.entity.ai.control.FlyingMoveControl;
 import net.minecraft.world.entity.ai.navigation.FlyingPathNavigation;
 import net.minecraft.world.entity.ai.navigation.PathNavigation;
 import net.minecraft.world.level.Level;
@@ -12,7 +12,6 @@ import net.minecraft.world.phys.Vec3;
 import simplepets.brainsynder.api.entity.passive.IEntityAllayPet;
 import simplepets.brainsynder.api.pet.PetType;
 import simplepets.brainsynder.api.user.PetUser;
-import simplepets.brainsynder.nms.VersionTranslator;
 import simplepets.brainsynder.nms.entity.EntityPet;
 
 /**
@@ -22,6 +21,7 @@ import simplepets.brainsynder.nms.entity.EntityPet;
 public class EntityAllayPet extends EntityPet implements IEntityAllayPet {
     public EntityAllayPet(PetType type, PetUser user) {
         super(EntityType.ALLAY, type, user);
+        this.moveControl = new FlyingMoveControl(this, 20, true);
     }
 
     @Override
@@ -35,6 +35,12 @@ public class EntityAllayPet extends EntityPet implements IEntityAllayPet {
 
     @Override
     public void travel(Vec3 vec3) {
+        if (isOwnerRiding()) {
+            super.travel(vec3);
+            this.calculateEntityAnimation(this, false);
+            return;
+        }
+
         if (this.isInWater()) {
             this.moveRelative(0.02F, vec3);
             this.move(MoverType.SELF, this.getDeltaMovement());
@@ -44,17 +50,9 @@ public class EntityAllayPet extends EntityPet implements IEntityAllayPet {
             this.move(MoverType.SELF, this.getDeltaMovement());
             this.setDeltaMovement(this.getDeltaMovement().scale(0.5D));
         } else {
-            float f;
-            if (this.onGround) {
-                f = this.level.getBlockState(new BlockPos(this.getX(), this.getY() - 1.0D, this.getZ())).getBlock().getFriction() * 0.91F;
-            } else {
-                f = 0.91F;
-            }
-
-            float g = VersionTranslator.cube(0.6F) * VersionTranslator.cube(0.91F) / VersionTranslator.cube(f);
-            this.moveRelative(this.onGround ? 0.1F * g : 0.02F, vec3);
+            this.moveRelative(this.getSpeed(), vec3);
             this.move(MoverType.SELF, this.getDeltaMovement());
-            this.setDeltaMovement(this.getDeltaMovement().scale(f));
+            this.setDeltaMovement(this.getDeltaMovement().scale(0.9100000262260437D));
         }
 
         this.calculateEntityAnimation(this, false);
