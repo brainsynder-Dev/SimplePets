ce73efb4c0d43fba1df50652a23a43cd2e14bd81
brainsynder <briansnyder498@gmail.com>
2022-07-20 21:47:00 -0400

    Added a simple test command for the database command to clear out the NPCs in the database

--- MAIN/src/main/java/simplepets/brainsynder/commands/list/DatabaseCommand.java
+++ MAIN/src/main/java/simplepets/brainsynder/commands/list/DatabaseCommand.java
@@ -1,3 +1,5 @@
+
+
 package simplepets.brainsynder.commands.list;
 
 import lib.brainsynder.commands.annotations.ICommand;
@@ -24,8 +26,37 @@ public class DatabaseCommand extends PetSubCommand {
     }
 
     @Override
-    public void run(CommandSender sender) {
+    public void run(CommandSender sender, String[] args) {
+        if (args.length == 0) {
+            sendDatabaseInfo(sender);
+            return;
+        }
+
+        // This is a temp subcommand to test deleting NPCs from the database
+        if (args[0].equalsIgnoreCase("delete")) {
+            PlayerSQL.getInstance().removeNPCs();
+            return;
+        }
+
+        sendDatabaseInfo(sender);
+    }
+
+    private void sendDatabaseInfo (CommandSender sender) {
         PlayerSQL.getInstance().fetchRowCount(playerDataCount -> {
+            if (InventorySQL.getInstance() == null) {
+                sender.sendMessage(MessageFile.getTranslation(MessageOption.PREFIX)+Colorize.translateBungeeHex(" &#d1c9c9Player Data SQL &#b35349======&#de9790-------"));
+                sender.sendMessage(Colorize.translateBungeeHex(" &#e1eb5b- &#d1c9c9Type: &#e3c79a" + (PlayerSQL.getInstance().isUsingSqlite() ? "SQLite" : "MySQL")));
+                Tellraw raw = Tellraw.fromLegacy("&#e1eb5b - &#d1c9c9Status: ");
+                if (PlayerSQL.getInstance().isUsingSqlite()) {
+                    raw.then("CONNECTED").color(ChatColor.GREEN).tooltip("&7SQLite connections are kept connected");
+                }else{
+                    raw.then("IDLE").color("#e3aa4f").tooltip("&7MySQL connections are kept closed until they are needed","&7That's what the IDLE state is");
+                }
+                raw.send(sender);
+                sender.sendMessage(Colorize.translateBungeeHex(" &#e1eb5b- &#d1c9c9Players In Database: &#e3c79a"+playerDataCount));
+                return;
+            }
+
             InventorySQL.getInstance().fetchRowCount(invCount -> {
                 try {
                     sender.sendMessage(MessageFile.getTranslation(MessageOption.PREFIX)+Colorize.translateBungeeHex(" &#d1c9c9Player Data SQL &#b35349======&#de9790-------"));
@@ -50,3 +81,4 @@ public class DatabaseCommand extends PetSubCommand {
         });
     }
 }
+
