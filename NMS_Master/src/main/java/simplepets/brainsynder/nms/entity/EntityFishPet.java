93c08c93f56062ece28c7ffbb6039c7e4faa263d
Traks <traksag@protonmail.com>
2023-01-23 17:29:26 +0100

    Fix pet vanishing when clicked with bucket

--- NMS_Master/src/main/java/simplepets/brainsynder/nms/entity/EntityFishPet.java
+++ NMS_Master/src/main/java/simplepets/brainsynder/nms/entity/EntityFishPet.java
@@ -1,15 +1,23 @@
 package simplepets.brainsynder.nms.entity;
 
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.animal.Bucketable;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import simplepets.brainsynder.api.entity.misc.IEntityFishPet;
 import simplepets.brainsynder.api.pet.PetType;
 import simplepets.brainsynder.api.user.PetUser;
 
-public class EntityFishPet extends EntityPet implements IEntityFishPet {
+// Implement Bucketable so the server resends the entity when the client tries
+// to pick it up with a bucket
+public class EntityFishPet extends EntityPet implements IEntityFishPet, Bucketable {
     private static final EntityDataAccessor<Boolean> FROM_BUCKET;
 
     public EntityFishPet(EntityType<? extends Mob> entitytypes, PetType type, PetUser user) {
@@ -20,6 +28,34 @@ public class EntityFishPet extends EntityPet implements IEntityFishPet {
     protected void registerDatawatchers() {
         super.registerDatawatchers();
         this.entityData.define(FROM_BUCKET, false);
+    }
+
+    @Override
+    public boolean fromBucket() {
+        return entityData.get(FROM_BUCKET);
+    }
+
+    @Override
+    public void setFromBucket(boolean b) {
+        entityData.set(FROM_BUCKET, b);
+    }
+
+    @Override
+    public void saveToBucketTag(ItemStack itemStack) {
+    }
+
+    @Override
+    public void loadFromBucketTag(CompoundTag compoundTag) {
+    }
+
+    @Override
+    public ItemStack getBucketItemStack() {
+        return new ItemStack(Items.WATER_BUCKET);
+    }
+
+    @Override
+    public SoundEvent getPickupSound() {
+        return SoundEvents.BUCKET_FILL_FISH;
     }
 
     static {
