0e77ed4046ed3f664fb9f0cf6a8ac1ded63d83a1
brainsynder <briansnyder498@gmail.com>
2023-02-13 23:29:02 -0500

    Made the SQL connections only use 1 connection (seems to have fixed the majority of the issues)

--- MAIN/src/main/java/simplepets/brainsynder/sql/PlayerSQL.java
+++ MAIN/src/main/java/simplepets/brainsynder/sql/PlayerSQL.java
@@ -265,28 +265,38 @@ public class PlayerSQL extends SQLManager {
     }
 
     public CompletableFuture<Object> uploadData(PetUser user) {
-        return isPlayerInDatabase(user.getOwnerUUID()).thenApply(data -> {
-            if (data) {
-                return updateData(user.getOwnerUUID(), user.getOwnerName(), ((PetOwner) user).toCompound());
-            } else {
-                return insertData(user.getOwnerUUID(), user.getOwnerName(), ((PetOwner) user).toCompound());
-            }
-        });
+        StorageTagCompound compound = ((PetOwner) user).toCompound();
+
+        return CompletableFuture.<Void>supplyAsync(() -> {
+            Connection connection = implementConnection();
+            isPlayerInDatabase(connection, user.getOwnerUUID()).thenApply(data -> {
+                if (data) {
+                    return updateData(connection, user.getOwnerUUID(), user.getOwnerName(), compound);
+                } else {
+                    return insertData(connection, user.getOwnerUUID(), user.getOwnerName(), compound);
+                }
+            });
+            return null;
+        }, PetCore.getInstance().async).thenApplyAsync(result -> result, PetCore.getInstance().sync);
     }
 
     public boolean uploadDataSync(PetUser user) {
-        if (isPlayerInDatabaseSync(user.getOwnerUUID())) {
-            return updateDataSync(user.getOwnerUUID(), user.getOwnerName(), ((PetOwner) user).toCompound());
-        } else {
-            return insertDataSync(user.getOwnerUUID(), user.getOwnerName(), ((PetOwner) user).toCompound());
+        try (Connection connection = implementConnection()) {
+            if (isPlayerInDatabaseSync(connection, user.getOwnerUUID())) {
+                return updateDataSync(connection, user.getOwnerUUID(), user.getOwnerName(), ((PetOwner) user).toCompound());
+            } else {
+                return insertDataSync(connection, user.getOwnerUUID(), user.getOwnerName(), ((PetOwner) user).toCompound());
+            }
+        } catch (SQLException exception) {
+            return false;
         }
     }
 
     /**
      * Sync methods should be used for shutdown only
      */
-    public boolean updateDataSync(UUID uuid, String name, StorageTagCompound compound) {
-        try (Connection connection = implementConnection()) {
+    public boolean updateDataSync(Connection connection, UUID uuid, String name, StorageTagCompound compound) {
+        try {
             PreparedStatement statement = connection.prepareStatement("UPDATE `" + tablePrefix + "_players` SET " +
                     "name=?, UnlockedPets=?, PetName=?, NeedsRespawn=?, SavedPets=? WHERE uuid = ?");
             statement.setString(1, name);
@@ -304,8 +314,8 @@ public class PlayerSQL extends SQLManager {
         }
     }
 
-    public boolean insertDataSync(UUID uuid, String name, StorageTagCompound compound) {
-        try (Connection connection = implementConnection()) {
+    public boolean insertDataSync(Connection connection, UUID uuid, String name, StorageTagCompound compound) {
+        try {
             PreparedStatement statement = connection.prepareStatement("INSERT INTO `" + tablePrefix + "_players` " +
                     "(`uuid`, `name`, `UnlockedPets`, `PetName`, `NeedsRespawn`, `SavedPets`) VALUES (?, ?, ?, ?, ?, ?)");
             statement.setString(1, uuid.toString());
@@ -323,16 +333,13 @@ public class PlayerSQL extends SQLManager {
         }
     }
 
-    public boolean isPlayerInDatabaseSync(UUID uuid) {
-        try (Connection connection = implementConnection()) {
-            PreparedStatement statement = connection.prepareStatement("SELECT * FROM `" + tablePrefix + "_players` WHERE uuid = ?");
-            statement.setString(1, uuid.toString());
-            ResultSet results = statement.executeQuery();
-            boolean next = results.next();
-            results.close();
-            return next;
-        } catch (SQLException exception) {
-            exception.printStackTrace();
+    public boolean isPlayerInDatabaseSync(Connection connection, UUID uuid) {
+        try (PreparedStatement statement = connection.prepareStatement("SELECT * FROM `" + tablePrefix + "_players` WHERE uuid = '" + uuid.toString() + "' LIMIT 1")) {
+
+            ResultSet result = statement.executeQuery();
+
+            return result.next();
+        } catch (SQLException throwables) {
             return false;
         }
     }
@@ -368,20 +375,22 @@ public class PlayerSQL extends SQLManager {
         }
     }
 
-    public CompletableFuture<Boolean> updateData(UUID uuid, String name, StorageTagCompound compound) {
-        if (uuid.version() != 4) return CompletableFuture.completedFuture(false);
-        return CompletableFuture.supplyAsync(() -> updateDataSync(uuid, name, compound), PetCore.getInstance().async)
+    public CompletableFuture<Boolean> updateData(Connection connection, UUID uuid, String name, StorageTagCompound compound) {
+        // Prevents Offline UUIDs from being saved into the database
+        if (uuid.version()  != 4) return CompletableFuture.completedFuture(false);
+        return CompletableFuture.supplyAsync(() -> updateDataSync(connection, uuid, name, compound), PetCore.getInstance().async)
                 .thenApplyAsync(result -> result, PetCore.getInstance().sync);
     }
 
-    public CompletableFuture<Boolean> insertData(UUID uuid, String name, StorageTagCompound compound) {
-        if (uuid.version() != 4) return CompletableFuture.completedFuture(false);
-        return CompletableFuture.supplyAsync(() -> insertDataSync(uuid, name, compound), PetCore.getInstance().async)
+    public CompletableFuture<Boolean> insertData(Connection connection, UUID uuid, String name, StorageTagCompound compound) {
+        // Prevents Offline UUIDs from being saved into the database
+        if (uuid.version()  != 4) return CompletableFuture.completedFuture(false);
+        return CompletableFuture.supplyAsync(() -> insertDataSync(connection, uuid, name, compound), PetCore.getInstance().async)
                 .thenApplyAsync(result -> result, PetCore.getInstance().sync);
     }
 
-    public CompletableFuture<Boolean> isPlayerInDatabase(UUID uuid) {
-        return CompletableFuture.supplyAsync(() -> isPlayerInDatabaseSync(uuid), PetCore.getInstance().async)
+    public CompletableFuture<Boolean> isPlayerInDatabase(Connection connection, UUID uuid) {
+        return CompletableFuture.supplyAsync(() -> isPlayerInDatabaseSync(connection, uuid), PetCore.getInstance().async)
                 .thenApplyAsync(result -> result, PetCore.getInstance().sync);
     }
 }
