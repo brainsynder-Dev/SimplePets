628bcda04538f02581850da253e89c899a9cd7eb
brainsynder <briansnyder498@gmail.com>
2023-05-26 20:29:51 -0400

    Updated the Database command to use the new SQL code

--- MAIN/src/main/java/simplepets/brainsynder/commands/list/DatabaseCommand.java
+++ MAIN/src/main/java/simplepets/brainsynder/commands/list/DatabaseCommand.java
@@ -14,8 +14,7 @@ import simplepets.brainsynder.commands.Permission;
 import simplepets.brainsynder.commands.PetSubCommand;
 import simplepets.brainsynder.files.MessageFile;
 import simplepets.brainsynder.files.options.MessageOption;
-import simplepets.brainsynder.sql.InventorySQL;
-import simplepets.brainsynder.sql.PlayerSQL;
+import simplepets.brainsynder.sql.SQLData;
 
 import java.util.List;
 
@@ -50,21 +49,47 @@ public class DatabaseCommand extends PetSubCommand {
 
         // Clears the database of any NPC/offline UUIDs
         if (args[0].equalsIgnoreCase("removenpcs")) {
-            PlayerSQL.getInstance().removeNPCs(sender);
+            // Not Async yet...
+            PetCore.getInstance().getSqlHandler().removeNPCs().whenComplete((biOptional, throwable) -> {
+                int rawCount = biOptional.first().orElse(0);
+                int totalCount = biOptional.second().orElse(0);
+
+                if (rawCount == 0) {
+                    // No duplicates...
+                    sender.sendMessage(MessageFile.getTranslation(MessageOption.PREFIX) + ChatColor.GRAY + " No NPC account entries found that needed to be deleted");
+                    return;
+                }
+
+                sender.sendMessage(MessageFile.getTranslation(MessageOption.PREFIX) + ChatColor.GRAY + " Number of NPC accounts found: " + rawCount);
+                sender.sendMessage(MessageFile.getTranslation(MessageOption.PREFIX) + ChatColor.GRAY + " Number of accounts actually deleted: " + totalCount);
+            });
             return;
         }
 
         // Clears the database of all duplicate players
         if (args[0].equalsIgnoreCase("removeduplicates")) {
-            PlayerSQL.getInstance().removeDuplicates(sender);
+            PetCore.getInstance().getSqlHandler().removeDuplicates().whenComplete((biOptional, throwable) -> {
+                int rawCount = biOptional.first().orElse(0);
+                int totalCount = biOptional.second().orElse(0);
+
+                if (rawCount == 0) {
+                    // No duplicates...
+                    sender.sendMessage(MessageFile.getTranslation(MessageOption.PREFIX) + ChatColor.GRAY + " No duplicate entries found that needed to be deleted");
+                    return;
+                }
+
+                sender.sendMessage(MessageFile.getTranslation(MessageOption.PREFIX) + ChatColor.GRAY + " Number of duplicate accounts found: " + rawCount);
+                sender.sendMessage(MessageFile.getTranslation(MessageOption.PREFIX) + ChatColor.GRAY + " Number of duplicate entries actually deleted: " + totalCount);
+            });
             return;
         }
 
         // Fetches a list of all duplicate players in the database
         if (args[0].equalsIgnoreCase("findduplicates")) {
-            PlayerSQL.getInstance().findDuplicates(triples -> {
+            sender.sendMessage(MessageFile.getTranslation(MessageOption.PREFIX) + ChatColor.GRAY + " Finding any duplicates in the database...");
+            PetCore.getInstance().getSqlHandler().findDuplicates().whenComplete((triples, throwable) -> {
                 if (triples.isEmpty()) {
-                    sender.sendMessage(MessageFile.getTranslation(MessageOption.PREFIX)+ChatColor.GRAY+" No duplicate players were found in the database.");
+                    sender.sendMessage(MessageFile.getTranslation(MessageOption.PREFIX) + ChatColor.GRAY + " No duplicate players were found in the database.");
                     return;
                 }
 
@@ -75,8 +100,8 @@ public class DatabaseCommand extends PetSubCommand {
                 });
 
                 WebConnector.uploadPaste(SimplePets.getPlugin(), builder.toString(), s -> {
-                    sender.sendMessage(MessageFile.getTranslation(MessageOption.PREFIX)+ChatColor.GRAY+" Here is a list of duplicated players: ");
-                    sender.sendMessage(MessageFile.getTranslation(MessageOption.PREFIX)+ChatColor.GRAY+" "+s);
+                    sender.sendMessage(MessageFile.getTranslation(MessageOption.PREFIX) + ChatColor.GRAY + " Here is a list of duplicated players: ");
+                    sender.sendMessage(MessageFile.getTranslation(MessageOption.PREFIX) + ChatColor.GRAY + " " + s);
                 });
             });
             return;
@@ -85,43 +110,18 @@ public class DatabaseCommand extends PetSubCommand {
         sendDatabaseInfo(sender);
     }
 
-    private void sendDatabaseInfo (CommandSender sender) {
-        PlayerSQL.getInstance().fetchRowCount(playerDataCount -> {
-            if (InventorySQL.getInstance() == null) {
-                sender.sendMessage(MessageFile.getTranslation(MessageOption.PREFIX)+Colorize.translateBungeeHex(" &#d1c9c9Player Data SQL &#b35349======&#de9790-------"));
-                sender.sendMessage(Colorize.translateBungeeHex(" &#e1eb5b- &#d1c9c9Type: &#e3c79a" + (PlayerSQL.getInstance().isUsingSqlite() ? "SQLite" : "MySQL")));
-                Tellraw raw = Tellraw.fromLegacy("&#e1eb5b - &#d1c9c9Status: ");
-                if (PlayerSQL.getInstance().isUsingSqlite()) {
-                    raw.then("CONNECTED").color(ChatColor.GREEN).tooltip("&7SQLite connections are kept connected");
-                }else{
-                    raw.then("IDLE").color("#e3aa4f").tooltip("&7MySQL connections are kept closed until they are needed","&7That's what the IDLE state is");
-                }
-                raw.send(sender);
-                sender.sendMessage(Colorize.translateBungeeHex(" &#e1eb5b- &#d1c9c9Players In Database: &#e3c79a"+playerDataCount));
-                return;
+    private void sendDatabaseInfo(CommandSender sender) {
+        PetCore.getInstance().getSqlHandler().getRowCount().whenComplete((playerDataCount, throwable) -> {
+            sender.sendMessage(MessageFile.getTranslation(MessageOption.PREFIX) + Colorize.translateBungeeHex(" &#d1c9c9Player Data SQL &#b35349======&#de9790-------"));
+            sender.sendMessage(Colorize.translateBungeeHex(" &#e1eb5b- &#d1c9c9Type: &#e3c79a" + (SQLData.USE_SQLITE ? "SQLite" : "MySQL")));
+            Tellraw raw = Tellraw.fromLegacy("&#e1eb5b - &#d1c9c9Status: ");
+            if (SQLData.USE_SQLITE) {
+                raw.then("CONNECTED").color(ChatColor.GREEN).tooltip("&7SQLite connections are kept connected");
+            } else {
+                raw.then("IDLE").color("#e3aa4f").tooltip("&7MySQL connections are kept closed until they are needed", "&7That's what the IDLE state is");
             }
-
-            InventorySQL.getInstance().fetchRowCount(invCount -> {
-                try {
-                    sender.sendMessage(MessageFile.getTranslation(MessageOption.PREFIX)+Colorize.translateBungeeHex(" &#d1c9c9Player Data SQL &#b35349======&#de9790-------"));
-                    sender.sendMessage(Colorize.translateBungeeHex(" &#e1eb5b- &#d1c9c9Type: &#e3c79a" + (PlayerSQL.getInstance().isUsingSqlite() ? "SQLite" : "MySQL")));
-                    Tellraw raw = Tellraw.fromLegacy(" &#e1eb5b- &#d1c9c9Status: ");
-                    if (PlayerSQL.getInstance().isUsingSqlite()) {
-                        raw.then("CONNECTED").color(ChatColor.GREEN).tooltip("&7SQLite connections are kept connected");
-                    }else{
-                        raw.then("IDLE").color("#e3aa4f").tooltip("&7MySQL connections are kept closed until they are needed","&7That's what the IDLE state is");
-                    }
-                    raw.send(sender);
-                    sender.sendMessage(Colorize.translateBungeeHex(" &#e1eb5b- &#d1c9c9Players In Database: &#e3c79a"+playerDataCount));
-                    sender.sendMessage(MessageFile.getTranslation(MessageOption.PREFIX)+Colorize.translateBungeeHex(" &#d1c9c9Inventory Data SQL &#b35349====&#de9790------"));
-                    sender.sendMessage(Colorize.translateBungeeHex(" &#e1eb5b- &#d1c9c9Type: &#e3c79aSQLite"));
-                    Tellraw.fromLegacy(" &#e1eb5b- &#d1c9c9Status: ").then("CONNECTED").color(ChatColor.GREEN).tooltip("&7SQLite connections are kept connected").send(sender);
-                    sender.sendMessage(Colorize.translateBungeeHex(" &#e1eb5b- &#d1c9c9Inventories In Database: &#e3c79a"+invCount));
-                }catch (Exception e) {
-                    sender.sendMessage(MessageFile.getTranslation(MessageOption.PREFIX)+ChatColor.RED + "Failed to fetch database information");
-                    sender.sendMessage(ChatColor.RED + e.getMessage());
-                }
-            });
+            raw.send(sender);
+            sender.sendMessage(Colorize.translateBungeeHex(" &#e1eb5b- &#d1c9c9Players In Database: &#e3c79a" + playerDataCount));
         });
     }
 }
