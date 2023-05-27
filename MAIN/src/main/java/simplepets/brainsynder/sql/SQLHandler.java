c068aed1da263778f1e9c1b78ecf0fb7244b7aa6
brainsynder <briansnyder498@gmail.com>
2023-05-26 22:07:43 -0400

    Added some documentation

--- MAIN/src/main/java/simplepets/brainsynder/sql/SQLHandler.java
+++ MAIN/src/main/java/simplepets/brainsynder/sql/SQLHandler.java
@@ -28,16 +28,78 @@ public interface SQLHandler {
             "`SavedPets` " + getStupidTextThing() + " NOT NULL" +
             ")";
 
+    /**
+     * This function returns an implementation of the Connection interface in Java.
+     *
+     * @return A connection object is being returned.
+     */
     Connection implementConnection();
 
     void createTable();
 
+    /**
+     * This function sends player data, identified by UUID and name, along with a StorageTagCompound object, and returns a
+     * CompletableFuture<Boolean>.
+     *
+     * @param uuid UUID stands for Universally Unique Identifier. It is a 128-bit value that is used to identify
+     * information in computer systems. In this case, it is used to identify a specific player in the game.
+     * @param name The name parameter is a String that represents the name of the player whose data is being sent.
+     * @param compound `compound` is a `StorageTagCompound` object that represents a collection of key-value pairs, similar
+     * to a dictionary or a map. It is commonly used in Minecraft modding to store and retrieve data associated with game
+     * entities or items. In this context, it likely contains data related to a player
+     * @return The method `sendPlayerData` returns a `CompletableFuture<Boolean>`. This is a Java class that represents a
+     * future result of an asynchronous computation that will eventually produce a `Boolean` value. The `Boolean` value
+     * indicates whether the player data was successfully sent or not.
+     */
     CompletableFuture<Boolean> sendPlayerData(UUID uuid, String name, StorageTagCompound compound);
+
+    /**
+     * The function returns a CompletableFuture object that fetches a StorageTagCompound object associated with a given
+     * UUID.
+     *
+     * @param uuid UUID stands for Universally Unique Identifier. It is a 128-bit value used for identifying information in
+     * computer systems. In this context, the UUID is likely being used to identify a specific user or entity for which
+     * data is being fetched.
+     * @return A `CompletableFuture` object that will eventually contain a `StorageTagCompound` object after the data for
+     * the specified UUID has been fetched.
+     */
     CompletableFuture<StorageTagCompound> fetchData(UUID uuid);
+
+    /**
+     * The function returns a CompletableFuture object that will eventually contain an integer representing the row count.
+     *
+     * @return A `CompletableFuture` object that will eventually contain an `Integer` value representing the row count. The
+     * `CompletableFuture` is a way to perform asynchronous programming in Java, allowing the caller to continue executing
+     * other tasks while waiting for the row count to be calculated.
+     */
     CompletableFuture<Integer> getRowCount ();
 
+    /**
+     * The function returns a CompletableFuture that removes duplicates from a pair of integers and returns them as a
+     * BiOptional.
+     *
+     * @return The method `removeDuplicates()` is returning a `CompletableFuture` that will eventually hold a `BiOptional`
+     * object containing two `Integer` values.
+     */
     CompletableFuture<BiOptional<Integer, Integer>> removeDuplicates();
+
+    /**
+     * The function `removeNPCs()` returns a `CompletableFuture` that resolves to a `BiOptional` object containing two
+     * integers.
+     *
+     * @return The method `removeNPCs()` is returning a `CompletableFuture` that will eventually hold a `BiOptional` object
+     * containing two `Integer` values.
+     */
     CompletableFuture<BiOptional<Integer, Integer>> removeNPCs();
+
+    /**
+     * The function returns a CompletableFuture that finds and returns a list of Triple objects containing UUID, String,
+     * and Integer values representing duplicates.
+     *
+     * @return The method `findDuplicates()` returns a `CompletableFuture` that will eventually contain a `List` of
+     * `Triple` objects. Each `Triple` contains a `UUID`, a `String`, and an `Integer`. The `List` will contain all the
+     * duplicates found in the data.
+     */
     CompletableFuture<List<Triple<UUID, String, Integer>>> findDuplicates ();
 
     /**
@@ -51,6 +113,15 @@ public interface SQLHandler {
         return SQLData.USE_SQLITE ? "TEXT" : "LONGTEXT";
     }
 
+    /**
+     * This Java function loads various pet-related data from a SQL database and stores it in a StorageTagCompound.
+     *
+     * @param uuid The UUID of a player whose data is being loaded from a database.
+     * @param results A ResultSet object containing the data retrieved from the database query.
+     * @param syncLogs A boolean value indicating whether to synchronize the logging output. If set to true, the
+     * logging output will be synchronized across multiple threads.
+     * @return A StorageTagCompound object.
+     */
     default StorageTagCompound rowToCompound(UUID uuid, ResultSet results, boolean syncLogs) throws SQLException {
         StorageTagCompound compound = new StorageTagCompound();
 
